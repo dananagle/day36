12fff819816b45922f87a934643c97796ccb319c
dananagle <dananagle@Danas-MacBook-Air.local>
2015-06-24 09:19:06 -0500

    Initial commit

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,13 @@
+class PagesController < ApplicationController
+  require 'dotenv'
+  require 'http'
+  require 'pp'
+
+  Dotenv.load
+
+  before_action do
+   json = JSON.parse(Http.get("https://openapi.etsy.com/v2/listings/active?api_key=#{ENV.fetch("ETSY_API_KEY")}&limit=10&includes=MainImage"))
+      @data = json.to_hash
+      @info = @data["results"].map {|listing| listing}
+  end
+end
